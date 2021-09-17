491e17718fb1ac828f32a0e9cc0362022393710a
victor <victor14souza@gmail.com>
2021-09-16 23:41:17 -0300

    criando o backend com keycloack

---
+++ nestjs-api/src/accounts/dto/create-account.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateAccountDto {
+
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+  
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    subdomain: string;
+    
+}
---
+++ nestjs-api/src/transactions/transactions.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { TenantGuard } from 'src/tenant/tenant.guard';
+
+@UseGuards(JwtAuthGuard, TenantGuard)
+@Controller('transactions')
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Post()
+  create(@Body() createTransactionDto: CreateTransactionDto) {
+    return this.transactionsService.create(createTransactionDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.transactionsService.findAll();
+  }
+}
